463af42ea31e34fb344e31045e59b97426f30279
saltys <christopher.saltys@gmail.com>
2014-06-24 14:51:25 -0400

    Clean up indentation

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -12,19 +12,19 @@ I want to be able to create an account so that I can log in to the site
 # I am presented with a success message if I sign in successfully
 # I'm presented with errors if I provide invalid credentials
 
-scenario 'user inputs valid and required information' do
-  visit root_path
-  click_link 'Sign Up'
-  fill_in 'First Name', with: 'Harold'
-  fill_in 'Last Name', with: 'Bendegas'
-  fill_in 'Email', with: 'user@bendegas.com'
-  fill_in 'Password', with: 'password'
-  fill_in 'Password Confirmation', with: 'password'
-  click_button 'Sign Up'
+  scenario 'user inputs valid and required information' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: 'Harold'
+    fill_in 'Last Name', with: 'Bendegas'
+    fill_in 'Email', with: 'user@bendegas.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Sign Up'
 
-  expect(page).to have_content("Sign in successful")
-  expect(page).to have_content("Sign Out")
-end
+    expect(page).to have_content("Sign in successful")
+    expect(page).to have_content("Sign Out")
+  end
 
   scenario 'registration info not supplied' do
     visit root_path
@@ -33,8 +33,6 @@ end
 
     expect(page).to have_content("can't be blank")
     expect(page).to_not have_content("Sign Out")
-
-
   end
 
   scenario 'password does not match confirmation' do
@@ -49,8 +47,6 @@ end
 
     expect(page).to have_content("doesn't match")
     expect(page).to_not have_content("Sign Out")
-
-
   end
 
 end
