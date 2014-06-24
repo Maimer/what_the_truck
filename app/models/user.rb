199196f787ec33a2441dfa7605aa5d3c592ea979
Maimer <nesnl23@yahoo.com>
2014-06-24 11:51:24 -0400

    Added admin method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def is_admin?
+    role == "admin"
+  end
 end
