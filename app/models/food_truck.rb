dae3f1ac925f7986852967461f6e95992b2b4322
Maimer <nesnl23@yahoo.com>
2014-06-25 16:43:29 -0400

    Readded average rating method for FoodTruck class

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -4,4 +4,12 @@ class FoodTruck < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :user_id, presence: true
+
+  def average_rating
+    if reviews.size > 0
+      (reviews.pluck(:rating).sum / reviews.size.to_f).round(0).to_i
+    else
+      0
+    end
+  end
 end
