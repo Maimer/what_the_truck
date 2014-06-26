634a5960c97aabb739f31c863285de32791a7e68
Maimer <nesnl23@yahoo.com>
2014-06-26 19:00:38 -0400

    Fixed redirect after deleting a review so that it redirected to that review's food truck path instead of to the admin root path

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -3,12 +3,14 @@ class Admin::ReviewsController < ApplicationController
   before_action :authorize_user
 
   def destroy
-    if Review.find(params[:id]).destroy
+    review = Review.find(params[:id])
+    food_truck = review.food_truck
+    if review.destroy
       flash[:notice] = "Successfully deleted review."
     else
       flash[:alert] = "Failed to remove review."
     end
-    redirect_to admin_root_path
+    redirect_to admin_food_truck_path(food_truck)
   end
 
   def authorize_user
