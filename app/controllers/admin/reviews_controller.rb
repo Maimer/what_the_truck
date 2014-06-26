e9cf230626515be7c42ea07d8dd32c7b810b1f3e
Maimer <nesnl23@yahoo.com>
2014-06-26 17:49:44 -0400

    Added truck show page and individual delete links for reviews.

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -3,7 +3,12 @@ class Admin::ReviewsController < ApplicationController
   before_action :authorize_user
 
   def destroy
-
+    if Review.find(params[:id]).destroy
+      flash[:notice] = "Successfully deleted review."
+    else
+      flash[:alert] = "Failed to remove review."
+    end
+    redirect_to admin_root_path
   end
 
   def authorize_user
