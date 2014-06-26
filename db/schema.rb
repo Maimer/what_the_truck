8315ba411255801424383c945f9723b03f4f172a
Maimer <nesnl23@yahoo.com>
2014-06-25 21:22:04 -0400

    Added carrierwave so that users can upload profile photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625195849) do
+ActiveRecord::Schema.define(version: 20140626011459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 20140625195849) do
   end
 
   add_index "food_trucks", ["name"], name: "index_food_trucks_on_name", unique: true, using: :btree
-
-  create_table "pg_search_documents", force: true do |t|
-    t.text     "content"
-    t.integer  "searchable_id"
-    t.string   "searchable_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "reviews", force: true do |t|
     t.integer  "rating",                    null: false
@@ -62,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140625195849) do
     t.string   "first_name",                             null: false
     t.string   "last_name",                              null: false
     t.boolean  "admin",                  default: false, null: false
+    t.string   "profile_photo"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
