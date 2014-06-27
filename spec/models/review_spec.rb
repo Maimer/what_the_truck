11eb8622efb6d7c14a8ae5e79dbfc172ace4be17
saltys <christopher.saltys@gmail.com>
2014-06-27 10:37:54 -0400

    Remove user spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
 require 'spec_helper'
 
-# RSpec.describe Review, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
 
 describe Review do
-  user = FactoryGirl.create(:user, id: 1)
+  user = FactoryGirl.create(:user)
   review = FactoryGirl.create(:review)
 
   it "sends an email" do
