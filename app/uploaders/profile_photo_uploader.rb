d621045becef139c4706b0d4c8893fe98bb8c761
Maimer <nesnl23@yahoo.com>
2014-06-27 16:18:58 -0400

    test image upload

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -4,7 +4,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,6 +14,12 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_to_fill: [500, 300]
+
+  version :thumb do
+    process resize_to_fill: [170, 100]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
