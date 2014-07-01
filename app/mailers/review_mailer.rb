6a66730e64c3af2e68e02271edc3cb592062e880
saltys <christopher.saltys@gmail.com>
2014-07-01 09:42:44 -0400

    Styling

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -4,6 +4,7 @@ class ReviewMailer < ActionMailer::Base
   def new_review_email(review)
     @user = User.find(review.food_truck.user_id)
     @food_truck = review.food_truck
+    attachments.inline['title.png'] = File.read(Rails.root.join("app/assets/images/title.png"))
     attachments.inline['photo.png'] = File.read(Rails.root.join("public/#{@food_truck.truck_photo.url(:thumb)}"))
     mail(to: @user.email, subject: "New review on #{review.food_truck.name}!")
   end
