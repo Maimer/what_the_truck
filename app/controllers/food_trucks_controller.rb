232cc498b75bf159656ef578afcc047f3657588d
Maimer <nesnl23@yahoo.com>
2014-07-01 14:10:39 -0400

    Added google maps iframe

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -13,6 +13,23 @@ class FoodTrucksController < ApplicationController
   def show
     @food_truck = FoodTruck.find(params[:id])
     @reviews = @food_truck.reviews.order(votes_count: :desc).page params[:page]
+
+    # Breakfast: 6-10AM; Lunch 10AM-3PM; Dinner: 3-11PM; Late Night: 11PM-12AM
+    hour = Time.now.hour
+    meal = "None"
+    if hour >= 5 && hour < 9
+      meal = "Breakfast"
+    elsif hour >= 9 && hour < 15
+      meal = "Lunch"
+    elsif hour >= 15 && hour < 23
+      meal = "Dinner"
+    elsif hour >= 23
+      meal = "Late Night"
+    end
+
+    @location = Location.where(food_truck_id: @food_truck.id,
+                               day_of_week: Time.now.strftime('%A'),
+                               time_of_day: meal).first.coordinates
   end
 
   def new
